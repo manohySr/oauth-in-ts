2167a7a28dfe2c5a7b941f0517c6286c831a0870
manohySr <manohyrajaonah@gmail.com>
2024-07-02 21:34:18 +0400

    feat: 	add html file for user interaction 	wip api endpoint for the authentication

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,32 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import path from "path";
+require("dotenv").config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+function checkLoggedIn(req: Request, res: Response, next: NextFunction) {
+  const isLoggedIn = true;
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: "you must log in!",
+    });
+  }
+  next();
+}
+
+app.get("/auth/google", (req: Request, res: Response) => {});
+
+app.get("/auth/google/callback", (req: Request, res: Response) => {});
+
+app.get("/auth/logout", (req: Request, res: Response) => {});
+
+app.get("/secret", checkLoggedIn, (req: Request, res: Response) => {
+  res.send("the secret message is: BIZOU");
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../", "public", "index.html"));
+});
+
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
